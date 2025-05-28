2579244910ad781816cc001724192e689df56e7a
Matheus Serra <ggmat304@gmail.com>
2025-05-28 08:52:07 -0300

    Rotas arrumadas de novo

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,7 +7,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 function Router() {
 
   return (
-    <BrowserRouter basename="/Pagina-React-SadDog">
+    <BrowserRouter basename="/Pagina-React-SadDog/dist">
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projetos" element={<Projetos/>}/>
